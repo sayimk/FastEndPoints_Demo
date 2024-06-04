66f4be1e4e9b0f2882d8e17f4a3cac4de3a3cb6e
Mohammed Khan <mohammedkhan@msks-air.lan>
2024-06-04 23:36:49 +0100

    working endpoint

--- FastEndPoints_Demo/FastEndPoints_Demo/Program.cs
+++ FastEndPoints_Demo/FastEndPoints_Demo/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using FastEndpoints;
+using FastEndpoints.Swagger;
 
-app.MapGet("/", () => "Hello World!");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddFastEndpoints()
+    .SwaggerDocument();
+
+var app = builder.Build();
+app.UseFastEndpoints()
+    .UseSwaggerGen()
+    .UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FastEndpoints Demo");
+        c.RoutePrefix = string.Empty;
+    });
 
 app.Run();
